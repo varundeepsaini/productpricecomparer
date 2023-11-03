37f9140b0ae1911913d7879d63b4c04a5824892b
Varun Deep Saini <varun.23bcs10048@ms.sst.scaler.com>
2023-11-03 08:40:59 +0530

    Fixed the wrong ids for the attributes

--- main.py
+++ main.py
@@ -3,26 +3,36 @@ from bs4 import BeautifulSoup
 
 
 def scrape_amazon_product(url):
-    req = requests.get(url)
-    if req.status_code != 200:
-        return {None: None}
+    session = requests.Session()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/95.0.4638.69 Safari/537.36'}
 
-    soup = BeautifulSoup(req.text, 'html.parser')
+    response = session.get(url, headers=headers)
 
-    product_name = (soup.find('span', {'id': 'productTitle'}).get_text().strip())
-    product_price = soup.find('span', {'id': 'priceblock_ourprice'}).get_text().strip()
+    if response.status_code != 200:
+        return None
+    soup = BeautifulSoup(response.content, 'html.parser')
+    product_name = soup.find('span', id='productTitle')
+    product_price = soup.find('span', class_='a-price-whole')
 
-    data = {"Product Name": product_name, "Product Price": product_price}
-    return data
+    if product_name is not None:
+        product_name = product_name.text.strip()
+    else:
+        product_name = 'Product name not found'
+    if product_price is not None:
+        product_price = product_price.text.strip()
+    else:
+        product_price = 'Product price not found'
+    return {'Product Name': product_name, 'Product Price': product_price}
 
 
-if __name__ == "__main__":
-    product_url = input("Enter Amazon Product URL: ")
+if __name__ == '__main__':
+    product_url = input('Enter Amazon Product URL: ')
+
     result = scrape_amazon_product(product_url)
 
-    if list(result.keys())[0] is None:
-        print("Error: Request failed")
+    if result is not None:
+        print(f"Product Name: {result['Product Name']}")
+        print(f"Product Price: {result['Product Price']}")
     else:
-        for key, value in result.items():
-            print(f"{key}: {value}")
-
+        print('Error: Request failed or invalid URL')
