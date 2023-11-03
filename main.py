b709d929c6a735b81246b46b9fb08736666185aa
Varun Deep Saini <varun.23bcs10048@ms.sst.scaler.com>
2023-11-03 08:51:28 +0530

    Added Support for Flipkart

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def scrape_amazon_product(url):
-    session = requests.Session()
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/95.0.4638.69 Safari/537.36'}
+def scrape_flipkart_product(url):
 
-    response = session.get(url, headers=headers)
+    response = requests.get(url)
 
     if response.status_code != 200:
         return None
+
     soup = BeautifulSoup(response.content, 'html.parser')
-    product_name = soup.find('span', id='productTitle')
-    product_price = soup.find('span', class_='a-price-whole')
+
+    product_name = soup.find('span', class_='B_NuCI')
+    product_price = soup.find('div', class_='_30jeq3 _16Jk6d')
 
     if product_name is not None:
         product_name = product_name.text.strip()
@@ -26,10 +25,40 @@ def scrape_amazon_product(url):
     return {'Product Name': product_name, 'Product Price': product_price}
 
 
-if __name__ == '__main__':
-    product_url = input('Enter Amazon Product URL: ')
+def scrape_amazon_product(url):
+    session = requests.Session()
+    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/95.0.4638.69 Safari/537.36"}
 
-    result = scrape_amazon_product(product_url)
+    response = session.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return None
+    soup = BeautifulSoup(response.content, 'html.parser')
+    product_name = soup.find('span', id="productTitle")
+    product_price = soup.find('span', class_="a-price-whole")
+
+    if product_name is not None:
+        product_name = product_name.text.strip()
+    else:
+        product_name = "Product name not found"
+    if product_price is not None:
+        product_price = product_price.text.strip()
+    else:
+        product_price = "Product price not found"
+    return {'Product Name': product_name, 'Product Price': product_price}
+
+
+if __name__ == '__main__':
+
+    operations = int(input("Enter the website you want to scrape: \n1. Amazon\n2. Flipkart\n"))
+    result = None
+    if operations == 1:
+        product_url = input('Enter Amazon Product URL: ')
+        result = scrape_amazon_product(product_url)
+    elif operations == 2:
+        product_url = input('Enter Flipkart Product URL: ')
+        result = scrape_flipkart_product(product_url)
 
     if result is not None:
         print(f"Product Name: {result['Product Name']}")
